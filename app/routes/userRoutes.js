0e884cf30d3725168ad21a621fc4c74e58a3017b
Darma Cahya <darmady6@gmail.com>
2024-12-25 14:28:32 +0700

    update history jury

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -32,17 +32,29 @@ module.exports = function(app) {
     res.render("notFound");
   });
 
-  app.get("/history-jury",authJwt.verifyToken, async (req, res) => {
+  app.get("/history-judge",authJwt.verifyToken, authJwt.isAdminOrJury, async (req, res) => {
     try{
-      const userId = req.userId;
-      const scores = await Score.findAll({
+      const juryId = req.userId;
+      const scoresJury = await Score.findAll({
         where: {
           userId: userId
         }
       });
+      const eventsJury = await Event.findAll();
+      const userRole = req.user ? req.user.role : "guest";
+      res.render("History", {userRole, juryId, scoresJury, eventsJury});
+    } catch (error) {
+      res.status(500).render("error", { message: error.message }); 
+    }
+  });
+
+  app.get("/history-judges",authJwt.verifyToken, authJwt.isAdmin, async (req, res) => {
+    try{
+      const adminId = req.userId;
+      const scores = await Score.findAll();
       const events = await Event.findAll();
       const userRole = req.user ? req.user.role : "guest";
-      res.render("History", {userRole, userId, scores, events});
+      res.render("History", {userRole, adminId, scores, events, currentPath: req.path });
     } catch (error) {
       res.status(500).render("error", { message: error.message }); 
     }
