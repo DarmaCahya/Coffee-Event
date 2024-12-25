8b427bddd7b8eb75a18237a7b04aed949fd84a74
abi-widiarta <abiwidiarta03@gmail.com>
2024-12-25 13:56:02 +0700

    fix: adjust button color

--- app/models/coffeeModel.js
+++ app/models/coffeeModel.js
@@ -1,44 +1,51 @@
 module.exports = (sequelize, Sequelize) => {
-    const Coffee = sequelize.define("coffee_flavours", {
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+  const Coffee = sequelize.define("coffee_flavours", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
       },
-      coffee_beans: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      origin: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      flavours: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      acidity: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      body: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      aftertaste: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      sweetness: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      img: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-    });
-  
-    return Coffee;
-  };
-  
+      allowNull: false,
+    },
+    coffee_beans: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    origin: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    flavours: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    acidity: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    aftertaste: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    sweetness: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    img: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  });
+
+  return Coffee;
+};
